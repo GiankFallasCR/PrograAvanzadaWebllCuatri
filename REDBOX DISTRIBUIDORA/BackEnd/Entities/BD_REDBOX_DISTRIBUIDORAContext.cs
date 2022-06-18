562ea2d802e50c70a815028ec139ea482c0bbbea
Roberto Cordoba <robertocordoba@outlook.com>
2022-06-18 10:01:21 -0600

    String Con Rob Mac

--- REDBOX DISTRIBUIDORA/BackEnd/Entities/BD_REDBOX_DISTRIBUIDORAContext.cs
+++ REDBOX DISTRIBUIDORA/BackEnd/Entities/BD_REDBOX_DISTRIBUIDORAContext.cs
@@ -31,7 +31,7 @@ namespace BackEnd.Entities
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 //  optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=BD_REDBOX_DISTRIBUIDORA;Integrated Security=True;Trusted_Connection=True;"); conex JoseR
-                // optionsBuilder.UseSqlServer("Server=localhost\\MSSQL;Database=BD_REDBOX_DISTRIBUIDORA;Integrated Security=True;Trusted_Connection=True;");
+                 optionsBuilder.UseSqlServer("data source=localhost,1433;Database=BD_REDBOX_DISTRIBUIDORA;User Id=sa;Password=yourStrong(!)Password;");
             }
         }
 
