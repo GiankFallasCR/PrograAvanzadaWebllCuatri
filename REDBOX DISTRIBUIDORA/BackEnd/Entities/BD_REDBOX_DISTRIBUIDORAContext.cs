20a8ccdac8d105f51c8e88102787af68a79d04b4
Jose Redondo <jredondo10321@ufide.ac.cr>
2022-06-23 13:53:39 -0600

    string de conexion jose

--- REDBOX DISTRIBUIDORA/BackEnd/Entities/BD_REDBOX_DISTRIBUIDORAContext.cs
+++ REDBOX DISTRIBUIDORA/BackEnd/Entities/BD_REDBOX_DISTRIBUIDORAContext.cs
@@ -30,7 +30,7 @@ namespace BackEnd.Entities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("data source=192.168.0.137,1433;Database=BD_REDBOX_DISTRIBUIDORA;User Id=sa;Password=Fidelitas123;");
+                /*String de conexion JOSE*/optionsBuilder.UseSqlServer("data source=192.168.0.137,1433;Database=BD_REDBOX_DISTRIBUIDORA;User Id=sa;Password=Fidelitas123;");
             }
         }
 
