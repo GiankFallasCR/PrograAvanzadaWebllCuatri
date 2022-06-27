ddc85c1c976b75d3d7011e9dbae4ffee44218d1c
Giancarlo Fallas <fallassolis10@gmail.com>
2022-06-26 19:34:49 -0600

    Agregado String de conexion giank y actualizacion de frontEnd

--- REDBOX DISTRIBUIDORA/BackEnd/Entities/BD_REDBOX_DISTRIBUIDORAContext.cs
+++ REDBOX DISTRIBUIDORA/BackEnd/Entities/BD_REDBOX_DISTRIBUIDORAContext.cs
@@ -31,8 +31,10 @@ namespace BackEnd.Entities
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 /*String de conexion JOSE DOCKER*//*optionsBuilder.UseSqlServer("data source=localhost,1433;Database=BD_REDBOX_DISTRIBUIDORA;User Id=sa;Password=Fidelitas123;");*/
-                /*String de conexion Rob*//*optionsBuilder.UseSqlServer("data source=localhost,1433;Database=BD_REDBOX_DISTRIBUIDORA;User Id=sa;Password=yourStrong(!)Password;");*/
-                /*String de conexion Drix*/ optionsBuilder.UseSqlServer("Server=DESKTOP-LFFMPOQ\\SQLEXPRESS;Database=BD_REDBOX_DISTRIBUIDORA;Integrated Security=True;Trusted_Connection=True;");
+                /*String de conexion Robe*//*optionsBuilder.UseSqlServer("data source=localhost,1433;Database=BD_REDBOX_DISTRIBUIDORA;User Id=sa;Password=yourStrong(!)Password;");*/
+                /*String de conexion Drix optionsBuilder.UseSqlServer("Server=DESKTOP-LFFMPOQ\\SQLEXPRESS;Database=BD_REDBOX_DISTRIBUIDORA;Integrated Security=True;Trusted_Connection=True;");*/
+                /*String de conexion Gianke*/ optionsBuilder.UseSqlServer("Server=LAPTOP-KONS7N9P\\SQLEXPRESS;Database=BD_REDBOX_DISTRIBUIDORA;Integrated Security=True;Trusted_Connection=True;");
+
             }
         }
 
