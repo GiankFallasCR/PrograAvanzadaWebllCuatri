5839a4620b9054ce5007884c2b294e88da4392ec
dariomonestel <monestel00@gmail.com>
2022-07-16 17:29:07 -0600

    AÑADIR LOS PROYECTOS API Y CAMBIAR STRING DE CONEX.

---
+++ REDBOX DISTRIBUIDORA/BackEnd/DAL/Utilities/Util.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd.Utilities
+{
+    public static class Util
+    {
+        public static string ConnectionString { get; set; }
+    }
+}
