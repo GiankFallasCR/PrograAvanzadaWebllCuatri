d0652cb529751df7bd2787635e1763bcf381df36
dariomonestel <monestel00@gmail.com>
2022-07-18 15:57:25 -0600

    IMPLEMENTAR INTERFAZ DE AUTENTICACIÓN.

--- REDBOX DISTRIBUIDORA/ClienteAPI/Program.cs
+++ REDBOX DISTRIBUIDORA/ClienteAPI/Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Usuarios}/{action=InicioSesion}/{id?}");
 
 app.Run();
