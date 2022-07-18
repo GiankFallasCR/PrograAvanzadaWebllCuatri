bf88e3848be0ed7079d7fbc54b3fb72126f8b6b8
dariomonestel <monestel00@gmail.com>
2022-07-18 16:32:59 -0600

    UsuariosController agregado

---
+++ REDBOX DISTRIBUIDORA/ClienteAPI/Controllers/UsuariosController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClienteAPI.Controllers
+{
+    public class UsuariosController : Controller
+    {
+
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        // GET: UsuariosController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UsuariosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuariosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
