1c131e10f4d3fab098f6874ecea2e35c33b5d25b
Jose Redondo <jredondo10321@ufide.ac.cr>
2022-07-25 15:27:06 -0600

    cambios

--- REDBOX DISTRIBUIDORA/FrontEnd/Help/UsuarioModel.cs
+++ REDBOX DISTRIBUIDORA/FrontEnd/Help/UsuarioModel.cs
@@ -1,6 +1,69 @@
-﻿namespace FrontEnd.Help
+﻿using System.Net;
+using FrontEnd.Models;
+using Newtonsoft.Json;
+
+
+namespace FrontEnd.Help
 {
     public class UsuarioModel
     {
+
+
+        public UsuarioViewModel BuscarClientePorCedula(string identificacion)
+        {
+            UsuarioViewModel usuarioobj = new UsuarioViewModel();
+
+
+            string direccion = "https://api.hacienda.go.cr/fe/ae";
+            string ruta = direccion + "?identificacion=" + identificacion;
+            string respuesta = GetHttp(ruta);
+
+            try
+            {
+
+
+                var Datos = JsonConvert.DeserializeObject<UsuarioViewModel>(respuesta);
+
+                string nombreH = Datos?.Nombre;
+                usuarioobj.Nombre = nombreH.ToString();
+                usuarioobj.Cedula = identificacion.ToString();
+
+
+                return usuarioobj;
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+
+
+
+        }
+
+        public static string GetHttp(string ruta)
+        {
+
+            try
+            {
+                WebRequest request = (WebRequest)WebRequest.Create(ruta);
+                WebResponse response = (WebResponse)request.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+                return sr.ReadToEnd().Trim();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+
     }
+
+
 }
+
