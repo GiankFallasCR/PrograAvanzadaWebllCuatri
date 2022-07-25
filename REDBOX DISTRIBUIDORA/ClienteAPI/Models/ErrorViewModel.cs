6605bf1d22a4b160ee802b2c6f2017d1ea0cf4e6
Jose Redondo <jredondo10321@ufide.ac.cr>
2022-07-25 14:23:50 -0600

    Eliminacion de carpetas no necesarias

--- REDBOX DISTRIBUIDORA/ClienteAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClienteAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
